91a64513d91c9e84fae3dce79347c729e6d867eb
fabriciog <fgb4101@gmail.com>
2020-05-20 14:58:55 -0600

    Completed module

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+
+const app = express();
+
+app.use('views', path.join(__dirname, 'src', 'views'));
+app.use('view engine', 'ejs');
+app.use('/public', path.join(__dirname, 'src', 'public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Index'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('PS Project Running on port 3000!');
+});
