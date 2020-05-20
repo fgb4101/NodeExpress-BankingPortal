8be236c1cfa70d77c35e6f640af87f53b498927c
fabriciog <fgb4101@gmail.com>
2020-05-20 15:39:41 -0600

    fixed module 1

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const express = require('express')
 
 const app = express();
 
-app.use('views', path.join(__dirname, 'src', 'views'));
-app.use('view engine', 'ejs');
-app.use('/public', path.join(__dirname, 'src', 'public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index', {
